eb5dfa92fbc2d108b369f4ff443139f0162d29d0
Alejandro Cepeda <acepeda19@usf.edu>
2022-06-16 17:29:26 -0400

    Create Module2.py

---
+++ Module2.py
@@ -0,0 +1,6 @@
+# double quote wrapped string to the console
+print("Hi fellow data enthusiasts!")
+
+# single quote use along with print() function
+greeting = 'Welcome to my Blog!'
+print("String wrapped in single quotes:", greeting)
